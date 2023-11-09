b8c0e2752f7bb3b2972e1cdf6bbd3338e0c0847c
gafrong <gafrong@gmail.com>
2023-11-09 16:45:36 +0900

    onboarding clean up

--- src/app/onboarding/page.jsx
+++ src/app/onboarding/page.jsx
@@ -6,10 +6,6 @@ export default function Page() {
     return (
         <div className="pl-5 pt-5">
             <Onboardingtrack />
-            <div>Shop name</div>
-            <div>username</div>
-            <div>Description</div>
-            <div>Phone number</div>
             <Link href="/onboarding/shopname">name</Link>
         </div>
     );
--- src/components/Onboadingtrack.jsx
+++ src/components/Onboadingtrack.jsx
@@ -1,11 +1,50 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import StepProgressBar from 'react-step-progress';
 import 'react-step-progress/dist/index.css';
 
 const Onboardingtrack = () => {
-    const step1Content = <h1 className="mt-20">Step 1 content Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias optio, sit eaque unde dolorem ullam omnis vero. Corrupti, nesciunt quisquam aut placeat, modi reiciendis cumque, veniam sit est vero fugit?</h1>;
+    const [brand, setBrand] = useState('');
+    const step1Content = 
+        <div className="mt-20">
+            <div className="flex flex-row">
+                <label
+                    htmlFor="brand"
+                    className="block text-sm font-medium leading-6 text-gray-900 m-3.5"
+                >
+                    브랜드 / 상점 이름
+                </label>
+                <div className="mt-2">
+                    <input
+                        onChange={(e) => setBrand(e.target.value)}
+                        id="brand"
+                        name="text"
+                        type="text"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6 pl-3"
+                    />
+                </div>
+            </div>
+            <div className="flex flex-row">
+                <label
+                    htmlFor="brand"
+                    className="block text-sm font-medium leading-6 text-gray-900 m-3.5"
+                >
+                    브랜드 / 상점 이름
+                </label>
+                <div className="mt-2">
+                    <input
+                        onChange={(e) => setBrand(e.target.value)}
+                        id="brand"
+                        name="text"
+                        type="text"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6 pl-3"
+                    />
+                </div>
+            </div>
+        </div>;
     const step2Content = <h1 className="mt-20">Step 2 content</h1>;
     const step3Content = <h1 className="mt-20">Step 3 content</h1>;
 
@@ -26,6 +65,8 @@ const Onboardingtrack = () => {
             <StepProgressBar
                 startingStep={0}
                 onSubmit={onFormSubmit}
+                nextBtnName="다음"
+                previousBtnName="이전"
                 steps={[
                     {
                         label: 'Shop Info',
