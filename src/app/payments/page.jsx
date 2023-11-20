e0260453e11bb4a2a364717a99d72d4d7de8c09d
vast-coding <vast.coding@gmail.com>
2023-11-21 03:46:41 +0700

    added payments page

---
+++ src/app/payments/page.jsx
@@ -0,0 +1,25 @@
+import { DataTable } from "./data-table"
+import { columns } from "./columns";
+import { data } from "./data";
+// async function getData() {
+//   // Fetch data from your API here.
+//   return [
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     // ...
+//   ]
+// }
+
+export default async function DemoPage() {
+  // const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
