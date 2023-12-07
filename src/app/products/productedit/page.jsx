0f9c90049bf7431ea56594f189413c18c65c001f
gafrong <gafrong@gmail.com>
2023-12-07 14:59:18 +0900

    product detail working

--- src/app/products/productedit/page.jsx
+++ src/app/products/productedit/page.jsx
@@ -14,12 +14,10 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
-import { useRouter } from "next/navigation";
 
 export default function Page() {
     const user = useUserStore((state) => state.user);
     const [ products, setProducts ] = useState([]);
-    const router = useRouter();
     const vendorId = user?._id;
 
     const getVendorProduct = async () => {
@@ -37,12 +35,6 @@ export default function Page() {
         }
     }, [vendorId])
 
-    const handleEditProduct = (product) => {
-
-        console.log('checking handle edit product', product)
-        router.push('/products/productdetail', product);
-    };
-
     return (
         <div className="pl-5 pt-5">
             <h1>Product Edit Page</h1>
@@ -58,6 +50,7 @@ export default function Page() {
                 products.map((product, index) => (
        
                     <TableBody key={index}>
+                        {console.log('PROD', product)}
                         <TableCell>
                             <img src={awsURL+product.image} alt={`Product ${index}`} className="w-12 h-12 rounded-sm" />
                         </TableCell>
@@ -65,7 +58,12 @@ export default function Page() {
                             <div className="pl-5 mt-2">{product.name}</div>
                         </TableCell>
                         <TableCell>
-                            <button className="ml-5 p-4 pt-1 pb-1 border rounded-sm" onClick={()=>handleEditProduct(product)}>edit</button>
+                            <Link
+                                href={{
+                                    pathname: "/products/productdetail",
+                                    query: product
+                                }}
+                            >edit</Link>
                         </TableCell>
                     </TableBody>
               
