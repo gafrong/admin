ff47206e769cf7170d48193993efedb897a9f335
gafrong <gafrong@gmail.com>
2023-12-07 15:57:13 +0900

    product edit list

--- src/app/products/productdetail/page.jsx
+++ src/app/products/productdetail/page.jsx
@@ -10,12 +10,23 @@ export default function Page({searchParams}) {
     return (
         <div className="p-10">
             <img src={awsURL+product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.description}</p>
-            <p>{product.discount}</p>
-            <p>{product.price}</p>
-            <p>{product.richDescription}</p>
-            <p>{product.name}</p>
+            <div className="pt-5">
+                <p>제품명: {product.name}</p>
+                <p>설명: {product.description}</p>
+                <p>가격: {product.price}</p>
+                <p>배송비: {product.deliveryFeeAmount}</p>
+                <p>배송비 적용: {product.deliveryFee}</p>
+                <p>상세설명: {product.richDescription}</p>
+                <p>카테고리: {product.category}</p>
+                <p>드롭상품: {product.dropProduct}</p>
+                <p>할인판매: {product.onSale}</p>
+                <p>할인률: {product.discount}%</p>
+                <p>판매중: {product.display}</p>
+                <p>색옵션: {product.colorOptions}</p>
+                <p>옵션 1: {product.subOption1}</p>
+                <p>옵션 2: {product.subOption2}</p>
+                <p>옵션 3: {product.subOption3}</p>
+            </div>
         </div>
     );
 }
