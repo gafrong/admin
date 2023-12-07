9b838cda3480d8b28de0f876db8c07376fac7269
gafrong <gafrong@gmail.com>
2023-12-07 14:59:18 +0900

    product detail working

--- src/app/products/productdetail/page.jsx
+++ src/app/products/productdetail/page.jsx
@@ -1,13 +1,21 @@
 "use client";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-
-export default function Page(props) {
+import awsURL from "@/assets/common/awsUrl";
+export default function Page({searchParams}) {
     const router = useRouter();
     const data = router
-
-
-
-    console.log('DATA', props)
-    return <></>;
+    const product = searchParams;
+    console.log('DATA', searchParams)
+    return (
+        <div className="p-10">
+            <img src={awsURL+product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.description}</p>
+            <p>{product.discount}</p>
+            <p>{product.price}</p>
+            <p>{product.richDescription}</p>
+            <p>{product.name}</p>
+        </div>
+    );
 }
