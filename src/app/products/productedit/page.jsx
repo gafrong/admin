ef16b2166a962997f0cf72c60642ca206c6f323c
gafrong <gafrong@gmail.com>
2023-12-08 14:42:24 +0900

    added spinner to product edit

--- src/app/products/productedit/page.jsx
+++ src/app/products/productedit/page.jsx
@@ -14,19 +14,24 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Page() {
     const user = useUserStore((state) => state.user);
     const [ products, setProducts ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
     const vendorId = user?._id;
 
     const getVendorProduct = async () => {
         try {
+            setLoading(true);
             const response = await axios.get(`${baseURL}products/admin/${vendorId}`);
 
             setProducts(response.data);
         } catch (error ) {
             console.log('Product fetch error', error)
+        } finally {
+            setLoading(false);
         }
     }
     useEffect(()=> {
@@ -36,40 +41,46 @@ export default function Page() {
     }, [vendorId])
 
     return (
-        <div className="pl-5 pt-5">
-            <h1>Product Edit Page</h1>
-            <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[60px]">Product</TableHead>
-                            <TableHead className="w-[300px]">Title</TableHead>
-                            <TableHead>Edit</TableHead>
-                        </TableRow>
-                    </TableHeader>
-            {products?.length > 0 && (
-                products.map((product, index) => (
-       
-                    <TableBody key={index}>
-                        {console.log('PROD', product)}
-                        <TableCell>
-                            <img src={awsURL+product.image} alt={`Product ${index}`} className="w-12 h-12 rounded-sm" />
-                        </TableCell>
-                        <TableCell>
-                            <div className="pl-5 mt-2">{product.name}</div>
-                        </TableCell>
-                        <TableCell>
-                            <Link
-                                href={{
-                                    pathname: "/products/productdetail",
-                                    query: product
-                                }}
-                            >edit</Link>
-                        </TableCell>
-                    </TableBody>
-              
-                ))
-            )}
-              </Table>
-        </div>
+        <>
+        {loading ?
+            <LoadingSpinner/>
+        :
+            <div className="pl-5 pt-5">
+                <h1>Product Edit Page</h1>
+                <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[60px]">Product</TableHead>
+                                <TableHead className="w-[300px]">Title</TableHead>
+                                <TableHead>Edit</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                {products?.length > 0 && (
+                    products.map((product, index) => (
+        
+                        <TableBody key={index}>
+                            {console.log('PROD', product)}
+                            <TableCell>
+                                <img src={awsURL+product.image} alt={`Product ${index}`} className="w-12 h-12 rounded-sm" />
+                            </TableCell>
+                            <TableCell>
+                                <div className="pl-5 mt-2">{product.name}</div>
+                            </TableCell>
+                            <TableCell>
+                                <Link
+                                    href={{
+                                        pathname: "/products/productdetail",
+                                        query: product
+                                    }}
+                                >edit</Link>
+                            </TableCell>
+                        </TableBody>
+                
+                    ))
+                )}
+                </Table>
+            </div>
+        }
+        </>
     );
 }
