95e7da0f03cf053be8881e3f86435aa030d5ab4a
gafrong <gafrong@gmail.com>
2024-01-10 14:59:57 +0900

    added spinner to video submit

--- src/app/videos/videoadd/page.jsx
+++ src/app/videos/videoadd/page.jsx
@@ -16,6 +16,7 @@ import {
 } from '@/components/ui/dialog'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
+import LoadingSpinner from '@/components/LoadingSpinner'
 import useUserStore from '@/store/zustand'
 import axios from 'axios'
 import { Plus } from 'lucide-react'
@@ -29,6 +30,7 @@ export default function Page() {
     const token = useUserStore((state) => state?.token);
     const userId = user?._id;
 
+    const [loading, setLoading] = useState(false)
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false);
     const [isSelected, setIsSelected] = useState(false);
@@ -134,21 +136,18 @@ export default function Page() {
         setIsSelected(true);
     };
     
-    
-    
- 
-
   const handleDescriptionChange = (e) => {
     setDescription(e.target.value)
   }
 
-  const handleFirstProduct = (e) => {
-    console.log('ee', e)
-  }
-
   const handleSubmit =  async () => {
-    console.log('checking')
+    if (!videoFile || !thumbnail || !description || videoProductIds.length === 0) {
+        // Show an error message or prevent submission
+        alert('모든 정보를 정확히 선택/입력하시기 바랍니다.');
+        return;
+    }
     try {
+        setLoading(true);
         const formData = new FormData();
         
         formData.append('video', videoFile);
@@ -179,7 +178,7 @@ export default function Page() {
                                 setVideoProductIds([]);
                                 setDescription('');
                                 setThumbnail(null);
-                                console.log('RES DATA', res.data)
+                                setLoading(false);
                                 router.push('/videos/video-add-complete')
                             } else {
                                 alert("영상을 올릴 수 없습니다");
@@ -192,11 +191,13 @@ export default function Page() {
                             setVideoProductIds([]);
                             setDescription('');
                             setThumbnail(null);
+                            setLoading(false);
                         });
         
         // axios call
     } catch (error) {
-        console.error('Error uploading video:', error)
+        console.error('Error uploading video:', error);
+        setLoading(false);
     }
   }
 
@@ -216,111 +217,117 @@ export default function Page() {
   }, [videoProducts])
 
   return (
-    <div className="pl-5 pt-5">
-      <h1 className="mb-10 text-2xl">동영상 등록</h1>
-      <div className="pl-5">
-        <h2 className="mb-5">1. 동영상 파일 선택</h2>
+    <>
+        {loading ?
+            <LoadingSpinner />
+        :
+        <div className="pl-5 pt-5">
+        <h1 className="mb-10 text-2xl">동영상 등록</h1>
         <div className="pl-5">
-          <div>
-            <div className={`flex items-center space-x-2 ${styles.btn}`}>
-              <Plus className="h-4 w-4" />
-              <Label htmlFor="video" className={styles.pointer}>
-                동영상 파일 선택
-              </Label>
-            </div>
-            <input
-                type="file"
-                accept="video/*"
-                onChange={pickVideo}
-                id="video"
-                className="hidden"
-            />
-          </div>
-          {/* {thumbnail && <img src={thumbnail} alt="Video Thumbnail"  className={styles.thumbnail}/>} */}
-          {videoRef && (
-            <video ref={videoRef} className={styles.selectedVideo} />
-          )}
-          {isSelected ?
-            <div className="mt-5">
-              <p className="text-xs">파일명: {selectedFile.name}</p>
-              <p className="text-xs">파일타입: {selectedFile.type}</p>
-              <p className="text-xs">
-                파일 사이즈: {(selectedFile.size / 1000000).toFixed(2)}mb
-              </p>
-            </div>
-          : <p className="mt-5 text-xs">
-              (동영상 파일은 .mp4, .mov, .avi로 가능하며 사이즈는 50mb 이하로
-              업로드 가능합니다.)
-            </p>
-          }
-        </div>
-
-        <h2 className="mb-5 mt-10">2. 동영상 설명</h2>
-        <div className="pl-5">
-          <Textarea
-            value={description}
-            onChange={handleDescriptionChange}
-            placeholder="예) 스트레이트하고 와이드한 실루엣이 특징적인 데님"
-            className="mt-1 w-96"
-          />
-        </div>
-        <h2 className="mb-5 mt-10">3. 동영상 관련 상품 선택</h2>
-        <div className="flex pl-5">
-          {videoProducts.length > 0 &&
-            videoProducts.map((product, index) => (
-              <div key={index} className={styles.selectedProductImages}>
-                <img
-                  src={awsURL + product.image}
-                  alt={`Product ${index}`}
-                  className={styles.productImg}
-                />
-              </div>
-            ))}
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="secondary" className={styles.border}>
+            <h2 className="mb-5">1. 동영상 파일 선택</h2>
+            <div className="pl-5">
+            <div>
+                <div className={`flex items-center space-x-2 ${styles.btn}`}>
                 <Plus className="h-4 w-4" />
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>동영상 상품</DialogTitle>
-                <DialogDescription>
-                  동영상과 연관된 상품(들)을 선택하세요
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid max-h-[500px] gap-4 overflow-y-auto py-4">
-                {productList?.map((product, index) => (
-                  <div key={index} className={styles.productItem}>
-                    <div className={styles.checkbxContainer}>
-                      <Checkbox
-                        id={product.id}
-                        className={styles.checkbox}
-                        onClick={() => handleProductSelect(product, index)}
-                      />
-                    </div>
+                <Label htmlFor="video" className={styles.pointer}>
+                    동영상 파일 선택
+                </Label>
+                </div>
+                <input
+                    type="file"
+                    accept="video/*"
+                    onChange={pickVideo}
+                    id="video"
+                    className="hidden"
+                />
+            </div>
+            {/* {thumbnail && <img src={thumbnail} alt="Video Thumbnail"  className={styles.thumbnail}/>} */}
+            {videoRef && (
+                <video ref={videoRef} className={styles.selectedVideo} />
+            )}
+            {isSelected ?
+                <div className="mt-5">
+                <p className="text-xs">파일명: {selectedFile.name}</p>
+                <p className="text-xs">파일타입: {selectedFile.type}</p>
+                <p className="text-xs">
+                    파일 사이즈: {(selectedFile.size / 1000000).toFixed(2)}mb
+                </p>
+                </div>
+            : <p className="mt-5 text-xs">
+                (동영상 파일은 .mp4, .mov, .avi로 가능하며 사이즈는 50mb 이하로
+                업로드 가능합니다.)
+                </p>
+            }
+            </div>
+
+            <h2 className="mb-5 mt-10">2. 동영상 설명</h2>
+            <div className="pl-5">
+            <Textarea
+                value={description}
+                onChange={handleDescriptionChange}
+                placeholder="예) 스트레이트하고 와이드한 실루엣이 특징적인 데님"
+                className="mt-1 w-96"
+            />
+            </div>
+            <h2 className="mb-5 mt-10">3. 동영상 관련 상품 선택</h2>
+            <div className="flex pl-5">
+            {videoProducts.length > 0 &&
+                videoProducts.map((product, index) => (
+                <div key={index} className={styles.selectedProductImages}>
                     <img
-                      src={awsURL + product.image}
-                      alt={`Product ${index}`}
-                      className={styles.productImg}
+                    src={awsURL + product.image}
+                    alt={`Product ${index}`}
+                    className={styles.productImg}
                     />
-                  </div>
+                </div>
                 ))}
-              </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type="button">확인</Button>
-                </DialogClose>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+            <Dialog>
+                <DialogTrigger asChild>
+                <Button variant="secondary" className={styles.border}>
+                    <Plus className="h-4 w-4" />
+                </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>동영상 상품</DialogTitle>
+                    <DialogDescription>
+                    동영상과 연관된 상품(들)을 선택하세요
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid max-h-[500px] gap-4 overflow-y-auto py-4">
+                    {productList?.map((product, index) => (
+                    <div key={index} className={styles.productItem}>
+                        <div className={styles.checkbxContainer}>
+                        <Checkbox
+                            id={product.id}
+                            className={styles.checkbox}
+                            onClick={() => handleProductSelect(product, index)}
+                        />
+                        </div>
+                        <img
+                        src={awsURL + product.image}
+                        alt={`Product ${index}`}
+                        className={styles.productImg}
+                        />
+                    </div>
+                    ))}
+                </div>
+                <DialogFooter>
+                    <DialogClose asChild>
+                    <Button type="button">확인</Button>
+                    </DialogClose>
+                </DialogFooter>
+                </DialogContent>
+            </Dialog>
+            </div>
+            <div className="mt-10 pl-5">
+            <Button className="mt-5" onClick={handleSubmit}>
+                업로드
+            </Button>
+            </div>
         </div>
-        <div className="mt-10 pl-5">
-          <Button className="mt-5" onClick={handleSubmit}>
-            업로드
-          </Button>
         </div>
-      </div>
-    </div>
+        }
+    </>
   )
 }
