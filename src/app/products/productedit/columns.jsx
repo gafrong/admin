fceace654260ab12a32f8380f24c40e5f15f4375
vast-coding <vast.coding@gmail.com>
2024-01-27 23:56:56 +0700

    feat(productedit)

---
+++ src/app/products/productedit/columns.jsx
@@ -0,0 +1,80 @@
+import { ButtonSortable } from '@/app/orders/manage/data-table-button-sorting'
+import awsURL from '@/assets/common/awsUrl'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FiEdit } from 'react-icons/fi'
+
+// Table components
+// -----------------------------------------------------------------------------
+
+// Name
+const HeaderName = ({ column }) => (
+  <ButtonSortable column={column}>Title</ButtonSortable>
+)
+
+// Image
+const CellProductImage = ({ row }) => (
+  <div className="h-12 w-12 overflow-hidden rounded-sm border">
+    <Image
+      src={awsURL + row.getValue('image')}
+      width={48}
+      height={48}
+      style={{ objectFit: 'contain' }}
+      alt="product image"
+    />
+  </div>
+)
+
+const CellPrice = ({ row }) => {
+  const amount = parseFloat(row.getValue('price'))
+  // Format the amount as a dollar amount
+  const formatted = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'KRW',
+  }).format(amount)
+  return <div className="w-20 text-right font-medium">{formatted}</div>
+}
+// 500,000원 ?
+
+const CellEdit = ({ row }) => (
+  <Button variant="secondary" asChild>
+    <Link
+      href={{
+        pathname: '/products/productdetail',
+        query: { product: JSON.stringify(row.original) },
+      }}
+      className="w-40"
+    >
+      <span className="inline-flex w-20 flex-row gap-1">
+        <FiEdit className="mt-[2px] w-6" />
+        Edit
+      </span>
+    </Link>
+  </Button>
+)
+
+// Table configuration
+// -------------------
+
+export const columns = [
+  {
+    accessorKey: 'image',
+    cell: CellProductImage,
+    header: 'Product',
+  },
+  {
+    accessorKey: 'name',
+    header: HeaderName,
+  },
+  {
+    accessorKey: 'price',
+    cell: CellPrice,
+    // filterFn: filterPrice,
+    header: 'Price',
+  },
+  {
+    cell: CellEdit,
+    header: 'Edit',
+  },
+]
