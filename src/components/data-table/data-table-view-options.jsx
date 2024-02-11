408d074ab504423549209e21093ab9574f0a41db
vast-coding <vast.coding@gmail.com>
2024-02-12 06:06:12 +0700

    fix(responsive): make view button visible on mobile

--- src/components/data-table/data-table-view-options.jsx
+++ src/components/data-table/data-table-view-options.jsx
@@ -15,7 +15,7 @@ export function DataTableViewOptions({ table }) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="ml-auto hidden h-10 md:flex">
+        <Button variant="outline" className="ml-auto h-10 md:flex">
           <MixerHorizontalIcon className="mr-2 h-4 w-4" />
           View
         </Button>
