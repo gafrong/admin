f85375c55a810de9cc8a9c67e13480f13972ef72
vast-coding <vast.coding@gmail.com>
2024-02-14 12:14:48 +0700

    feat(date range): factor components, change to korean

--- src/components/data-table/data-table-date-range-picker.jsx
+++ src/components/data-table/data-table-date-range-picker.jsx
@@ -56,21 +56,63 @@ export function filterDateBetween(rows, id, filterValues) {
   return false
 }
 
-// function to get the date range for a month, used by date picker.
-
-const getDateFirstLastOfMonth = (date) => {
-  const today = date
-  const firstDateOfMonth = new Date(format(today, 'yyyy-MM-01'))
-  const lastDateOfMonth = new Date(format(lastDayOfMonth(today), 'yyyy-MM-dd'))
-  const monthName = format(firstDateOfMonth, 'LLLL')
-  return { from: firstDateOfMonth, to: lastDateOfMonth, label: monthName }
-}
-
 const today = new Date()
 
 const initialDateRange = {
   from: addDays(today, -365),
   to: today,
+}
+
+const PastDateRangesDropdown = ({ setDateRange }) => (
+  <>
+    <Select
+      onValueChange={(value) => {
+        const from = addDays(today, -parseInt(value))
+        setDateRange({ from, to: today })
+      }}
+    >
+      <div className="flex w-full">
+        <SelectTrigger className="SELECT_TRIGGER_! m-4 flex-auto">
+          <SelectValue placeholder="선택하다" />
+        </SelectTrigger>
+      </div>
+      <SelectContent position="popper">
+        <SelectItem value="0">오늘</SelectItem>
+        <SelectItem value="7">지난 주</SelectItem>
+        <SelectItem value="31">지난달</SelectItem>
+        <SelectItem value="93">지난 3개월</SelectItem>
+        <SelectItem value="365">지난 해</SelectItem>
+        <SelectItem value="730">지난 2년</SelectItem>
+      </SelectContent>
+    </Select>
+  </>
+)
+
+const ButtonCalendarTrigger = ({ date }) => {
+  const dateFrom = date?.from && format(date.from, 'LLL dd, y')
+  const dateTo = date?.to && format(date.to, 'LLL dd, y')
+  const ButtonText =
+    dateFrom && dateTo ? `${dateFrom} - ${dateTo}`
+    : dateFrom ? dateFrom
+    : 'Pick a date'
+
+  return (
+    <>
+      <PopoverTrigger asChild>
+        <Button
+          id="date"
+          variant={'outline'}
+          className={cn(
+            'w-[300px] justify-start text-left font-normal',
+            !date && 'text-muted-foreground',
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {ButtonText}
+        </Button>
+      </PopoverTrigger>
+    </>
+  )
 }
 
 export function DateRangePicker({ table, dateColumnId }) {
@@ -103,52 +145,15 @@ export function DateRangePicker({ table, dateColumnId }) {
     console.log('DATE', date)
   }
 
-  const dateFrom = date?.from && format(date.from, 'LLL dd, y')
-  const dateTo = date?.to && format(date.to, 'LLL dd, y')
-  const ButtonText =
-    dateFrom && dateTo ? `${dateFrom} - ${dateTo}`
-    : dateFrom ? dateFrom
-    : 'Pick a date'
-
   return (
     <div className="flex flex-wrap gap-4 rounded border p-2">
       <div className="mt-2">동영상 기간설정</div>
       <div className={cn('grid gap-2')}>
         <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              id="date"
-              variant={'outline'}
-              className={cn(
-                'w-[300px] justify-start text-left font-normal',
-                !date && 'text-muted-foreground',
-              )}
-            >
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              {ButtonText}
-            </Button>
-          </PopoverTrigger>
+          <ButtonCalendarTrigger date={date} />
           <PopoverContent className="w-auto p-0" align="start">
-            <Select
-              onValueChange={(value) => {
-                const from = addDays(today, -parseInt(value))
-                setDateRange({ from, to: today })
-              }}
-            >
-              <div className="mx-4 pb-2">
-                <SelectTrigger className="">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-              </div>
-              <SelectContent position="popper">
-                <SelectItem value="0">오늘</SelectItem>
-                <SelectItem value="7">지난 주</SelectItem>
-                <SelectItem value="31">지난달</SelectItem>
-                <SelectItem value="93">지난 3개월</SelectItem>
-                <SelectItem value="365">지난 해</SelectItem>
-                <SelectItem value="730">지난 2년</SelectItem>
-              </SelectContent>
-            </Select>
+            <PastDateRangesDropdown setDateRange={setDateRange} />
+
             <Calendar
               defaultMonth={date?.from}
               initialFocus
