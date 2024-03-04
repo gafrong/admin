64070f94aa692bd05f64da5f4d7d88c7d3615b8a
gafrong <gafrong@gmail.com>
2024-03-04 16:17:56 +0900

    checking broken korean

--- src/store/zustand.js
+++ src/store/zustand.js
@@ -4,9 +4,13 @@ const useUserStore = create((set) => ({
   user: null,
   token: null,
   isAuthenticated: false,
+  email: null,
   setUser: (userData, token) =>
     set({ user: userData, token: token, isAuthenticated: true }),
   clearUser: () => set({ user: null, token: null, isAuthenticated: false }),
+  setEmail: (email) => 
+    set({ email:email}),
+  clearEmail: () => set({ email: null}),
 }))
 
 export default useUserStore
