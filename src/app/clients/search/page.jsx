a9994100e58a75f24c4922e646e64fd0f34ab77d
vast-coding <vast.coding@gmail.com>
2024-03-21 10:11:48 +0700

    Refactor:  search page component to use TableClients component

--- src/app/clients/search/page.jsx
+++ src/app/clients/search/page.jsx
@@ -1,11 +1,11 @@
 import { PageTitle } from '@/components/typography/PageTitle'
-import { SearchCustomers } from './table-clients'
+import { TableClients } from './table-clients'
 
 export default function Page() {
   return (
     <div className="py-10 pl-5 pr-2">
       <PageTitle>Search Customer</PageTitle>
-      <SearchCustomers />
+      <TableClients />
     </div>
   )
 }
