5371f9763acd8855f99c92c725cf0f2e25f73c25
vast-coding <vast.coding@gmail.com>
2024-03-24 08:31:58 +0700

    Refactor table configuration and remove unused code

--- src/app/orders/manage/columns.jsx
+++ src/app/orders/manage/columns.jsx
@@ -229,32 +229,6 @@ const CellEditVendorNote = ({ column, row, table }) => {
   )
 }
 
-// CRUD update functions.
-// -----------------------------------------------------------------------------
-/*
-  After any CRUD operation, we want to update the table data, without refreshing the page or resetting the 
-  table component.
-  We do this by passing methods to the react-table meta function. This function will update a single item of the table data.
-*/
-
-export const updateTableData = ({ setTableData }) => ({
-  updateVendorNote: (rowIndex, columnId, value) => {
-    setTableData((prevData) =>
-      prevData.map((row, index) => {
-        if (index === rowIndex) {
-          const editedRow = {
-            ...prevData[rowIndex],
-            vendorNote: value,
-          }
-          return editedRow
-        }
-
-        return row
-      }),
-    )
-  },
-})
-
 // Table configuration
 // -------------------
 
