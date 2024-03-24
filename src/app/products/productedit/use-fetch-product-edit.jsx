2c95c796c3ed2de0a4d92dfeaef4eafa999bb98b
vast-coding <vast.coding@gmail.com>
2024-03-24 08:18:46 +0700

    Refactor useFetchProductEdit hook

--- src/app/products/productedit/use-fetch-product-edit.jsx
+++ src/app/products/productedit/use-fetch-product-edit.jsx
@@ -10,8 +10,7 @@ const fetcher = (url) => axios.get(url).then((res) => res.data)
 export function useFetchProductEdit() {
   const vendorId = useUserStore((state) => state.user)?._id
   const url = `${baseURL}products/admin/${vendorId}`
-  const { data, error } = useSWR(url, fetcher)
-  const isLoading = !data && !error
+  const { data, error, isLoading } = useSWR(url, fetcher)
 
   return { data, error, isLoading }
 }
