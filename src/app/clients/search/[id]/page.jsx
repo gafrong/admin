3293941404e2f3c8fdcde0e6115e94a8dbbf31d9
vast-coding <vast.coding@gmail.com>
2024-03-24 09:42:53 +0700

    Add TableClientDetails component for displaying client details

--- src/app/clients/search/[id]/page.jsx
+++ src/app/clients/search/[id]/page.jsx
@@ -1,14 +1,8 @@
 'use client'
 
 import { PageTitle } from '@/components/typography/PageTitle'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from '@/components/ui/table'
 import useUserStore from '@/store/zustand'
+import { TableClientDetails } from './table-client-details'
 import { TableOrderItems } from './table-order-items'
 
 export default function Page({ params }) {
@@ -20,50 +14,7 @@ export default function Page({ params }) {
   return (
     <div className="py-10 pl-5 pr-2">
       <PageTitle>Customer Information</PageTitle>
-
-      <div className="mb-10 overflow-hidden rounded-sm border">
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableHead className="w-40 bg-muted" scope="row">
-                Name
-              </TableHead>
-              <TableCell className="w-4/12">{client?.name}</TableCell>
-
-              <TableHead className="w-40 bg-muted" scope="row">
-                Id
-              </TableHead>
-              <TableCell>{client?._id}</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableHead className="w-40 bg-muted" scope="row">
-                Username
-              </TableHead>
-              <TableCell>{client?.username}</TableCell>
-
-              <TableHead className="w-40 bg-muted" scope="row">
-                Phone
-              </TableHead>
-              <TableCell>{client?.phone}</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableHead className="w-40 bg-muted" scope="row">
-                Address
-              </TableHead>
-              <TableCell colspan="4">{client?.address}</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableHead className="w-40 bg-muted" scope="row">
-                Email
-              </TableHead>
-              <TableCell colspan="4">{client?.email}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+      {client && <TableClientDetails client={client} />}
 
       <PageTitle>Order History</PageTitle>
       <TableOrderItems clientId={client?.id} />
