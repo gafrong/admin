856449a41c1da1bd9cf1315f67baef592da41750
vast-coding <vast.coding@gmail.com>
2024-03-24 12:46:02 +0700

    Refactor store and create SWR README.md

--- src/store/zustand.js
+++ src/store/zustand.js
@@ -4,14 +4,12 @@ const useUserStore = create((set) => ({
   clients: null,
   email: null,
   isAuthenticated: false,
-  orderItems: [],
   token: null,
   user: null,
   clearEmail: () => set({ email: null }),
   clearUser: () => set({ user: null, token: null, isAuthenticated: false }),
   setClients: (clients) => set({ clients }),
   setEmail: (email) => set({ email: email }),
-  setStoreOrderItems: (items) => set({ orderItems: items }),
   setUser: (userData, token) =>
     set({ user: userData, token: token, isAuthenticated: true }),
 }))
