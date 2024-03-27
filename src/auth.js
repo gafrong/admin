e2be273404e7b0549ee4714cdfe98741e78069a1
vast-coding <vast.coding@gmail.com>
2024-03-27 14:42:39 +0700

    Update authentication logic to use API endpoint for login

--- src/auth.js
+++ src/auth.js
@@ -1,23 +1,29 @@
+import axios from 'axios'
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import baseURL from './assets/common/baseUrl'
 
 const credentialsConfig = CredentialsProvider({
   name: 'Credentials',
   credentials: {
-    username: {
-      label: 'User Name',
-    },
-    password: {
-      label: 'Password',
-      type: 'password',
-    },
+    email: { label: 'Email', type: 'text' },
+    password: { label: 'Password', type: 'password' },
   },
   async authorize(credentials) {
-    if (credentials.username === 'sk' && credentials.password === '123')
-      return {
-        name: 'Vahid',
-      }
-    else return null
+    try {
+      // const user = { email: credentials.email, password: credentials.password }
+      const response = await axios.post(`${baseURL}admin/login`, credentials, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = response.data
+      if (data.user) return data.user
+      throw new Error('Invalid credentials')
+    } catch (error) {
+      throw new Error('Failed to login')
+    }
   },
 })
 
